783619a5aa924707266b952c5f3e88c0d1b3baa5
Talha Sajid <86770288+IamTalha-Sajid@users.noreply.github.com>
2022-03-30 16:02:13 +0500

    Update Voting.sol

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,112 +1,29 @@
-pragma solidity ^0.4.24;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.0;
+
+interface IERC721 {
+    function mintNFT(address recipient) external returns (uint256);
+}
 
 interface IERC20 {
-
-    function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function mintReward(address _userAdd) external returns (bool);
+    function voted(address _userAdd) external returns (bool);
+    function reward(address _userAdd) external returns (bool);
 }
 
+contract Voting {
 
-contract ERC20Basic is IERC20 {
-
-    string public constant name = "MyToken";
-    string public constant symbol = "MTK";
-    uint8 public constant decimals = 18;
-
-
-    mapping(address => uint256) balances;
-
-    mapping(address => mapping (address => uint256)) allowed;
-
-    uint256 totalSupply_ = 1 ether;
-
-
-   constructor() public{
-    balances[msg.sender] = totalSupply_;
-    }
-
-    function totalSupply() public view returns (uint256) {
-    return totalSupply_;
-    }
-
-    function mintReward(address _userAdd) public {
-        totalSupply_ += 5;
-        balances[_userAdd] = balances[_userAdd] + 5;
-    }
-
-    function balanceOf(address tokenOwner) public view returns (uint256) {
-        return balances[tokenOwner];
-    }
-
-    function transfer(address receiver, uint256 numTokens) public returns (bool) {
-        require(numTokens <= balances[msg.sender]);
-        balances[msg.sender] = balances[msg.sender]-numTokens;
-        balances[receiver] = balances[receiver]+numTokens;
-        emit Transfer(msg.sender, receiver, numTokens);
-        return true;
-    }
-
-    function approve(address delegate, uint256 numTokens) public returns (bool) {
-        allowed[msg.sender][delegate] = numTokens;
-        emit Approval(msg.sender, delegate, numTokens);
-        return true;
-    }
-
-    function allowance(address owner, address delegate) public view returns (uint) {
-        return allowed[owner][delegate];
-    }
-
-    function transferFrom(address owner, address buyer, uint256 numTokens) public returns (bool) {
-        require(numTokens <= balances[owner]);
-        require(numTokens <= allowed[owner][msg.sender]);
-
-        balances[owner] = balances[owner]-numTokens;
-        allowed[owner][msg.sender] = allowed[owner][msg.sender]+numTokens;
-        balances[buyer] = balances[buyer]+numTokens;
-        emit Transfer(owner, buyer, numTokens);
-        return true;
-    }
-}
-
-
-contract Voting is ERC20Basic {
-    address private owner;
-    uint public deadline;
-    bool public _timerstarted = false;
-    bool public _optionsadded = false;
-    mapping(address => option) options;
-    mapping(address => vote) votes;
-    mapping(address => voteCount) voteCounts;
+    IERC20 public token;
+    IERC721 public NFT;
     address[] voters;
-    address[] votersA;
-    address[] votersB;
-    address[] votersC;
-    address[] votersD;
-
-    constructor() public {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
 
     struct option {
-    address userId;
-    string optionA;
-    string optionB;
-    string optionC;
-    string optionD;
+        address userId;
+        string optionA;
+        string optionB;
+        string optionC;
+        string optionD;
     }
 
     struct vote{
@@ -115,24 +32,32 @@ contract Voting is ERC20Basic {
         bool hasVoted;
     }
 
-    struct voteCount{
-        address userId;
-        uint voteCountA;
-        uint voteCountB;
-        uint voteCountC;
-        uint voteCountD;
+    address private owner;
+    uint public deadline;
+    bool public _timerstarted = false;
+    bool public _optionsadded = false;
+    mapping(address => option) options;
+    mapping(address => vote) votes;
+
+    constructor() {
+        token = IERC20(0xC8f459782e3eD1DC403420b310743a19e401e665);
+        NFT = IERC721(0xAE71D2e0026ebaf10185Af5b588a161FDCb78d25);
+        owner = msg.sender;
     }
 
-    //Time will Start as soon as Function is Called
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
     function startTimer (uint _minute) public onlyOwner {
         require (_optionsadded == true, "Add the Options First");
         uint _seconds = _minute * 60;
         deadline=block.timestamp + _seconds;
         _timerstarted = true;
     }
-    
-    //Add Options for MCQs
-    function addOptions(address userId, string _optionA, string _optionB, string _optionC, string _optionD) public onlyOwner{
+
+    function addOptions(address userId, string memory _optionA, string memory _optionB, string memory _optionC, string memory _optionD) public onlyOwner{
         require(_timerstarted == false, "Timer has Already Started You cannot Change Option Now");
         options[userId] = option(userId, _optionA, _optionB, _optionC, _optionD);
         _optionsadded = true;
@@ -149,77 +74,74 @@ contract Voting is ERC20Basic {
         );
     }
 
-    function castVote(address _userId, string option) public returns (string) {
+    uint voteCountA = 0;
+    uint voteCountB = 0;
+    uint voteCountC = 0;
+    uint voteCountD = 0;
+
+    function castVote(address _userId, string memory _option) public {
         require(_optionsadded == true, "Options are Not Added Yet");
-        require( balanceOf(_userId) >= 20, "Token Balance is Less than 20");
+        require( token.balanceOf(_userId) >= 20, "Token Balance is Less than 20");
         require(_timerstarted == true, "Voting Time haven't Started Yet");
         require(votes[_userId].hasVoted == false, "You Have Already Voted");
-        if (keccak256(abi.encodePacked(option)) == keccak256(abi.encodePacked("A"))){
-            voteCounts[owner].voteCountA += 1;
+        if (keccak256(abi.encodePacked(_option)) == keccak256(abi.encodePacked("A"))){
             votes[_userId].hasVoted = true;
-            votersA.push(_userId);       
+            voteCountA++;    
         }
-        else if  (keccak256(abi.encodePacked(option)) == keccak256(abi.encodePacked("B"))){
-            voteCounts[owner].voteCountB += 1;
+        else if  (keccak256(abi.encodePacked(_option)) == keccak256(abi.encodePacked("B"))){
             votes[_userId].hasVoted = true;
-            votersB.push(_userId);       
+            voteCountB++;   
         }
-        else if (keccak256(abi.encodePacked(option)) == keccak256(abi.encodePacked("C"))){
-            voteCounts[owner].voteCountC += 1;
+        else if (keccak256(abi.encodePacked(_option)) == keccak256(abi.encodePacked("C"))){
             votes[_userId].hasVoted = true;
-            votersC.push(_userId);
+            voteCountC++;
         }
-        else if (keccak256(abi.encodePacked(option)) == keccak256(abi.encodePacked("D"))){
-            voteCounts[owner].voteCountD += 1;
+        else if (keccak256(abi.encodePacked(_option)) == keccak256(abi.encodePacked("D"))){
             votes[_userId].hasVoted = true;
-            votersD.push(_userId);
+            voteCountD++;
         }
-        else {
-            return ("Invalid Option! Please Select A, B, C or D");
-        }
-        votes[_userId] = vote(_userId, option, true);
-        balances[_userId] = balances[_userId] - 20;
+        votes[_userId] = vote(_userId, _option, true);
+        token.voted(_userId);
         voters.push(_userId);
     }
 
-    function checkWinner() public returns (string){
+    string winningOption;
+    function checkWinner() public {
         require(block.timestamp > deadline, "Voting Still in Progress");
-        if (voteCounts[owner].voteCountA > voteCounts[owner].voteCountB){
-            if (voteCounts[owner].voteCountA > voteCounts[owner].voteCountC){
-                if (voteCounts[owner].voteCountA > voteCounts[owner].voteCountD){
-                    for (uint i = 0; i<= votersA.length; i++){
-                        mintReward(votersA[i]);
-                    }
-                    return ("Option A is Winner and Awarded");
+        if (voteCountA > voteCountB){
+            if (voteCountA > voteCountC){
+                if (voteCountA > voteCountD){
+                    winningOption = "A";
                 }
                 else{
-                    for (i = 0; i<= votersD.length; i++){
-                        mintReward(votersD[i]);
-                    }
-                    return ("Option D is Winner and Awarded");
+                    winningOption = "D";
                 }
             }
         }
-        else if (voteCounts[owner].voteCountB > voteCounts[owner].voteCountC){
-            if (voteCounts[owner].voteCountB > voteCounts[owner].voteCountD){
-                for (i = 0; i<= votersB.length; i++){
-                        mintReward(votersB[i]);
-                    }
-                return ("Option B is Winner and Awarded");
+        else if (voteCountB > voteCountC){
+            if (voteCountB > voteCountD){
+                winningOption = "B";
             }
             else {
-                for (i = 0; i<= votersC.length; i++){
-                        mintReward(votersC[i]);
-                    }
-                return ("Option C is Winner and Awarded");
+                winningOption = "C";
             }
-        }
-        for (i = 0; i<= votersC.length; i++){
-            balances[voters[i]] = balances[voters[i]] + 20;
         }
     }
 
     function checkResult() public view onlyOwner returns (uint, uint, uint, uint){
-        return(voteCounts[owner].voteCountA, voteCounts[owner].voteCountB, voteCounts[owner].voteCountC, voteCounts[owner].voteCountD);
+        return(voteCountA, voteCountB, voteCountC, voteCountD);
+    }
+
+    function claimReward(address _userId) public returns (string memory){
+        require(block.timestamp > deadline, "Voting Still in Progress");
+            if (keccak256(abi.encodePacked(votes[_userId].option)) == keccak256(abi.encodePacked(winningOption))){
+                token.mintReward(_userId);
+                token.reward(_userId);
+                NFT.mintNFT(_userId);
+            }
+            else{
+                token.reward(_userId);
+            }
+        return ("Reward Sent Successfully");
     }
 }
