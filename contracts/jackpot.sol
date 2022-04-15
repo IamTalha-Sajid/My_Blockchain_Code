2cc6dbb8f35e73fbec5859dbdddfbbde1ac8de28
Talha Sajid <86770288+IamTalha-Sajid@users.noreply.github.com>
2022-04-15 10:55:47 +0500

    Added New Functions

--- contracts/jackpot.sol
+++ contracts/jackpot.sol
@@ -6,6 +6,12 @@ contract jackpot {
     address payable _DevWalletAddress;
     address public owner;
     mapping(address => uint256) public balances;
+    address[] users;
+    uint256 totalFunds;
+    bool jackpotIsActivated = false;
+    uint256 endingTime;
+    uint256 second;
+    address winner;
 
     constructor () {
         owner = msg.sender;
@@ -20,6 +26,12 @@ contract jackpot {
         _DevWalletAddress = devWallet;
     }
 
+    function startJackpot (uint256 _time) public onlyOwner {
+        jackpotIsActivated = true;
+        second = _time * 60;
+        endingTime = block.timestamp + second;
+    }
+
     function transferFundToDev (address payable walletAddress, uint256 amount) private returns (bool){
         walletAddress.transfer(amount);
         return true;
@@ -27,14 +39,29 @@ contract jackpot {
 
     function depositFunds (address payable walletAddress, uint256 amount) public payable {
         require (msg.value >= amount, "Insuficient Funds");
+        require (msg.sender != owner, "Admin Cannot Participate");
+        require (jackpotIsActivated == true, "Jackpot Not Started Yet");
 
         uint256 devFunds = (amount * 2)/100;
         uint256 withdrawalFunds = (amount * 3)/100;
         uint256 depositamount = (amount - (devFunds + withdrawalFunds));
 
-        walletAddress.transfer(depositamount);
+        totalFunds += depositamount;
         transferFundToDev(_DevWalletAddress, devFunds);
         balances[msg.sender] += withdrawalFunds;
+        users.push(msg.sender);
+    }
+
+    function pickWinner() public onlyOwner {
+        require (block.timestamp >= endingTime, "Jackpot Still in Progress");
+        require (jackpotIsActivated == true, "Jackpot Not Started Yet");
+
+        uint256 winningOption;
+
+        jackpotIsActivated = false;
+        winningOption = rand();
+        winner = users[winningOption];
+        balances[winner] += totalFunds;
     }
 
     function withdraw (uint256 amount, address payable desAdd) public {
@@ -42,5 +69,10 @@ contract jackpot {
 
         desAdd.transfer(amount);
         balances[msg.sender] -= amount;
+    }
+
+    function rand() internal view returns(uint Ran){
+        uint RanNum = uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender))) % users.length;
+        return RanNum;
     }
 }
