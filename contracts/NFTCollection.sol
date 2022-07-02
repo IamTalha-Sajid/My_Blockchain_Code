54e326c9a95f0b9f706a8a3e0d0efd4ee5e87cf0
IamTalha-Sajid <iamtalha.sajid@gmail.com>
2022-07-02 23:24:16 +0500

    Added NFT Collections Woring code

--- contracts/NFTCollection.sol
+++ contracts/NFTCollection.sol
@@ -1,116 +1,153 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
- 
-import "./token/ERC721/ERC721.sol";
-import "./token/ERC721/extensions/ERC721Enumerable.sol";
-import "./token/ERC721/extensions/ERC721Burnable.sol";
-import "./token/ERC721/extensions/ERC721URIStorage.sol";
-import "./access/AccessControlEnumerable.sol";
-import "./utils/Context.sol";
-import "./utils/Counters.sol";
-import "./access/Ownable.sol";
- 
- 
-contract KLB is Context,  AccessControlEnumerable, ERC721Enumerable, ERC721URIStorage, Ownable{
-  using Counters for Counters.Counter;
-  Counters.Counter public _tokenIdTracker;
- 
-  string private _baseTokenURI;
-  uint private _price;
-  uint private _max;
-  address _wallet;
- 
-  bool _openMint;
-  bool _openWhitelistMint;
- 
-  mapping(address => bool) private whitelist;
- 
-  constructor(string memory name, string memory symbol, string memory baseTokenURI, uint mintPrice, uint max, address wallet, address admin) ERC721(name, symbol) {
-      _baseTokenURI = baseTokenURI;
-      _price = mintPrice;
-      _max = max;
-      _wallet = wallet;
-      _openMint = false;
-      _openWhitelistMint = false;
-      _setupRole(DEFAULT_ADMIN_ROLE, wallet);
-      _setupRole(DEFAULT_ADMIN_ROLE, admin);
-  }
- 
-  function _baseURI() internal view virtual override returns (string memory) {
-      return _baseTokenURI;
-  }
- 
-  function setBaseURI(string memory baseURI) external {
-    require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "WBCyborgs: must have admin role to change base URI");
-    _baseTokenURI = baseURI;
-  }
- 
-  function setTokenURI(uint256 tokenId, string memory _tokenURI) external {
-    require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "WBCyborgs: must have admin role to change token URI");
-    _setTokenURI(tokenId, _tokenURI);
-  }
- 
-  function setPrice(uint mintPrice) external {
-    require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "WBCyborgs: must have admin role to change price");
-    _price = mintPrice;
-  }
- 
-  function setMint(bool openMint, bool openWhitelistMint) external {
-    require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "WBCyborgs: must have admin role to open/close mint");
-    _openMint = openMint;
-    _openWhitelistMint = openWhitelistMint;
-  }
- 
-  function price() public view returns (uint) {
-    return _price;
-  }
- 
-  function mint(address[] memory toSend ) public payable onlyOwner{
-    require(toSend.length <= 256, "WBCyborgs: max of 256 WBCyborgs per mint");
-    require(_openMint == true, "WBCyborgs: minting is closed");
-    require(msg.value == _price*toSend.length, "WBCyborgs: must send correct price");
-    require(_tokenIdTracker.current() + toSend.length <= _max, "WBCyborgs: not enough WBCyborgs left to be mint amount");
-    for(uint i = 0; i < toSend.length; i++) {
-      _mint(toSend[i], _tokenIdTracker.current());
-      _tokenIdTracker.increment();
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BillieJeans is ERC721Enumerable, Ownable {
+    using Strings for uint256;
+
+    string public baseURI;
+    string public baseExtension = ".json";
+    uint256 public cost = 0.00 ether;
+    uint256 public presaleCost = 0.03 ether;
+    uint256 public maxSupply = 1000;
+    uint256 public maxMintAmount = 1000;
+    bool public paused = false;
+    mapping(address => bool) public whitelisted;
+    mapping(address => bool) public presaleWallets;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory _initBaseURI
+    ) ERC721(_name, _symbol) {
+        setBaseURI(_initBaseURI);
+        mint(msg.sender, 20);
     }
-    payable(_wallet).transfer(msg.value);
-  }
- 
-  function mintWhitelist() public payable {
-    require(_openWhitelistMint == true, "WBCyborgs: minting is closed");
-    require(whitelist[msg.sender] == true, "WBCyborgs: user must be whitelisted to mint");
-    require(msg.value == _price, "WBCyborgs: must send correct price");
-    require(_tokenIdTracker.current() < _max, "WBCyborgs: all WBCyborgs have been minted");
-   
-    whitelist[msg.sender] = false;
-    _mint(msg.sender, _tokenIdTracker.current());
-    _tokenIdTracker.increment();
-    payable(_wallet).transfer(msg.value);
-  }
- 
-  function whitelistUser(address user) public {
-    require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "WBCyborgs: must have admin role to whitelist address");
-    whitelist[user] = true;
-  }
- 
-  function whitelistStatus(address user) public view returns(bool) {
-    return whitelist[user];
-  }
- 
-  function _burn(uint256 tokenId) internal virtual override(ERC721, ERC721URIStorage) {
-    return ERC721URIStorage._burn(tokenId);
-  }
- 
-  function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-    return ERC721URIStorage.tokenURI(tokenId);
-  }
- 
-  function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override(ERC721, ERC721Enumerable) {
-    super._beforeTokenTransfer(from, to, tokenId);
-  }
- 
-  function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControlEnumerable, ERC721, ERC721Enumerable) returns (bool) {
-    return super.supportsInterface(interfaceId);
-  }
+
+    // internal
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    // public
+    function mint(address _to, uint256 _mintAmount) public payable {
+        uint256 supply = totalSupply();
+        require(!paused);
+        require(_mintAmount > 0);
+        require(_mintAmount <= maxMintAmount);
+        require(supply + _mintAmount <= maxSupply);
+
+        if (msg.sender != owner()) {
+            if (whitelisted[msg.sender] != true) {
+                if (presaleWallets[msg.sender] != true) {
+                    //general public
+                    require(msg.value >= cost * _mintAmount);
+                } else {
+                    //presale
+                    require(msg.value >= presaleCost * _mintAmount);
+                }
+            }
+        }
+
+        for (uint256 i = 1; i <= _mintAmount; i++) {
+            _safeMint(_to, supply + i);
+        }
+    }
+
+    function walletOfOwner(address _owner)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+        for (uint256 i; i < ownerTokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return tokenIds;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory currentBaseURI = _baseURI();
+        return
+            bytes(currentBaseURI).length > 0
+                ? string(
+                    abi.encodePacked(
+                        currentBaseURI,
+                        tokenId.toString(),
+                        baseExtension
+                    )
+                )
+                : "";
+    }
+
+    //only owner
+    function setCost(uint256 _newCost) public onlyOwner {
+        cost = _newCost;
+    }
+
+    function setPresaleCost(uint256 _newCost) public onlyOwner {
+        presaleCost = _newCost;
+    }
+
+    function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
+        maxMintAmount = _newmaxMintAmount;
+    }
+
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
+
+    function setBaseExtension(string memory _newBaseExtension)
+        public
+        onlyOwner
+    {
+        baseExtension = _newBaseExtension;
+    }
+
+    function pause(bool _state) public onlyOwner {
+        paused = _state;
+    }
+
+    function whitelistUser(address _user) public onlyOwner {
+        whitelisted[_user] = true;
+    }
+
+    function removeWhitelistUser(address _user) public onlyOwner {
+        whitelisted[_user] = false;
+    }
+
+    function addPresaleUser(address _user) public onlyOwner {
+        presaleWallets[_user] = true;
+    }
+
+    function add100PresaleUsers(address[100] memory _users) public onlyOwner {
+        for (uint256 i = 0; i < 2; i++) {
+            presaleWallets[_users[i]] = true;
+        }
+    }
+
+    function removePresaleUser(address _user) public onlyOwner {
+        presaleWallets[_user] = false;
+    }
+
+    function withdraw() public payable onlyOwner {
+        (bool success, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(success);
+    }
 }
