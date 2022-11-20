93454519655bc8ccd4dda1bb060a9797a183bc15
IamTalha-Sajid <iamtalha.sajid@gmail.com>
2022-11-20 20:17:16 +0500

    Updated all the Files and Deleted useless Items

---
+++ test/AirDropTest.js
@@ -0,0 +1,43 @@
+describe("airDrop contract", function () {
+    it("Deployment should set the owner", async function () {
+      const [owner] = await ethers.getSigners();
+  
+      const airDrop = await ethers.getContractFactory("airDrop");
+  
+      const hardhatToken = await airDrop.deploy();
+
+      expect(await hardhatToken.checkOwner(owner)).to.equal(owner);
+    });
+
+    it("Address has been Added to the Contract", async function () {
+        const [owner] = await ethers.getSigners();
+    
+        const airDrop = await ethers.getContractFactory("airDrop");
+    
+        const hardhatToken = await airDrop.deploy();
+  
+        expect(await hardhatToken.checkOwner(owner)).to.equal(owner);
+      });
+
+      it("Transfer Tokens to all Addresses", async function () {
+        const [owner] = await ethers.getSigners();
+    
+        const airDrop = await ethers.getContractFactory("airDrop");
+    
+        const hardhatToken = await airDrop.deploy();
+  
+        expect(await hardhatToken.checkOwner(owner)).to.equal(owner);
+      });
+
+      it("Do not Transfer if Address is no Valid", async function () {
+        const [owner] = await ethers.getSigners();
+    
+        const airDrop = await ethers.getContractFactory("airDrop");
+    
+        const hardhatToken = await airDrop.deploy();
+  
+        expect(await hardhatToken.checkOwner(owner)).to.equal(owner);
+      });
+
+
+  });
