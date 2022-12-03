afedeb8bd365fcfbfc444f160953fc15ea4a3f72
IamTalha-Sajid <iamtalha.sajid@gmail.com>
2022-12-03 20:55:43 +0500

    Added Comments to the File

--- test/AirDropTest.js
+++ test/AirDropTest.js
@@ -1,3 +1,4 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
 describe("airDrop contract", function () {
     it("Deployment should set the owner", async function () {
       const [owner] = await ethers.getSigners();
@@ -8,7 +9,7 @@ describe("airDrop contract", function () {
 
       expect(await hardhatToken.checkOwner(owner)).to.equal(owner);
     });
-
+    ////////////////////////////////
     it("Address has been Added to the Contract", async function () {
         const [owner] = await ethers.getSigners();
     
